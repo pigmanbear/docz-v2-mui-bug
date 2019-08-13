72012e5e0be3b1fd29f4636c1274349f81bdf5b5
Austin Coose <austin.coose@radian.biz>
2019-08-12 22:23:51 -0600

    Working - docz not copying themes

---
+++ src/gatsby-theme-docz/theme/colors.js
@@ -0,0 +1,15 @@
+export const white = "#FFFFFF"
+export const grayUltraLight = "#FCFBFA"
+export const grayExtraLight = "#F5F6F7"
+export const grayLight = "#CED4DE"
+export const gray = "#7D899C"
+export const grayDark = "#2D3747"
+export const grayExtraDark = "#1D2330"
+export const dark = "#13161F"
+export const blueLight = "#e9f2fa"
+export const blue = "#355275"
+export const skyBlue = "#00bab3"
+export const negative = "#EB4D4B"
+
+export const tealLight = "#2fc4bf"
+
